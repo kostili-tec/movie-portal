9f6ebd8ac62d4c4d22f165352b6c6e02879433b3
kostili-tec <weh4teyou@gmail.com>
2024-04-20 17:21:49 +0300

    chore: install & config eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,61 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:react/recommended',
+    // 'plugin:i18next/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    // 'i18next',
+  ],
+
+  ignorePatterns: ['.eslintrc.сjs', 'webpack.config.ts', 'jest.config.ts'],
+  rules: {
+    'react/jsx-indent': [2, 2],
+    'react/jsx-indent-props': [2, 2],
+    indent: [2, 2],
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+    'react/jsx-props-no-spreading': 'off',
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': 'warn',
+    'react/require-default-props': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': 'off',
+    'no-shadow': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-underscore-dangle': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { varsIgnorePattern: '^_' }],
+    '@typescript-eslint/naming-convention': 'warn',
+    // 'i18next/no-literal-string': ['warn', { markupOnly: true }],
+  },
+  globals: {
+    __IS_DEV__: true,
+  },
+};
---
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -0,0 +1,18 @@
+import React, { useId } from 'react';
+import { useAppSelector } from '../../shared/hooks/redux';
+
+const ProfilePage = () => {
+  const apiKeyInputId = useId();
+  const {login, apikey} = useAppSelector(state => state.userReducser);
+  return (
+    <div>
+      <h2>{login}</h2>
+      <form action="">
+        <label htmlFor="apiKeyInputId"></label>
+        <input type="text" id='apiKeyInputId' defaultValue={apikey} />
+      </form>
+    </div>
+  )
+}
+
+export default ProfilePage;
