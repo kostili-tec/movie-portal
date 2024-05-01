b73e82ccc89cddff3035a6663464dab2b4ce5197
kostili-tec <weh4teyou@gmail.com>
2024-05-01 13:59:44 +0300

    feat: implement burger menu

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Hamburger from 'hamburger-react';
 import { AppLink } from '../../components/AppLink/AppLink';
 import ThemeSwitcher from '../../containers/ThemeSwitcher/ThemeSwitcher';
 import { useAppSelector, useAppDispatch } from '../../shared/hooks/redux';
 import { logoutUser } from '../../store/reducers/ActionsUser';
 import classes from './Header.module.scss';
+import { classNames } from '../../shared/lib/classNames';
 
 const Header = () => {
   const { isAuth, login } = useAppSelector((state) => state.userReducer);
+  const [isOpen, setOpen] = useState(false);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const handleLogout = () => {
@@ -17,33 +21,57 @@ const Header = () => {
 
   return (
     <header className={classes.header}>
-      <div className="logo">
-        <AppLink to="/">
-          <img
-            className={classes.logo}
-            src={`${process.env.PUBLIC_URL}/logo.png`}
-            alt="Movie portal"
-          />
-        </AppLink>
-      </div>
+      <div
+        className={
+          isOpen
+            ? classNames(classes.headerAdaptive, {}, [classes.headerAdaptiveOpen])
+            : classes.headerAdaptive
+        }
+      >
+        <div className="logo">
+          <AppLink to="/" onClick={() => setOpen(false)}>
+            <img
+              className={classes.logo}
+              src={`${process.env.PUBLIC_URL}/logo.png`}
+              alt="Movie portal"
+            />
+          </AppLink>
+        </div>
 
-      <div className={classes.headerAuth}>
-        {!isAuth ? (
-          <>
-            <AppLink to="/login">Login</AppLink>
-            <AppLink to="/sign_up">Sign Up</AppLink>
-          </>
-        ) : (
-          <>
-            <span className={classes.username}>{login}</span>
-            <AppLink to="/favorites">Favorites</AppLink>
-            <button className={classes.logout} type="button" onClick={handleLogout}>
-              Logout
-            </button>
-          </>
-        )}
-        <ThemeSwitcher />
+        <div className={classes.headerAuth}>
+          {!isAuth ? (
+            <>
+              <AppLink to="/login" onClick={() => setOpen(false)}>
+                Login
+              </AppLink>
+              <AppLink to="/sign_up" onClick={() => setOpen(false)}>
+                Sign Up
+              </AppLink>
+            </>
+          ) : (
+            <>
+              <span className={classes.username}>{login}</span>
+              <AppLink to="/favorites" onClick={() => setOpen(false)}>
+                Favorites
+              </AppLink>
+              <button className={classes.logout} type="button" onClick={handleLogout}>
+                Logout
+              </button>
+            </>
+          )}
+          <ThemeSwitcher />
+        </div>
       </div>
+      <div className={classes.burger}>
+        <Hamburger direction="left" toggled={isOpen} toggle={setOpen} />
+      </div>
+      <div
+        aria-hidden="true"
+        onClick={() => setOpen((prev) => !prev)}
+        className={
+          isOpen ? classNames(classes.overlay, {}, [classes.overlayOpen]) : classes.overlay
+        }
+      />
     </header>
   );
 };
